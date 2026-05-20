cc44baa48eb604f2f1408d2d80c88415472c4e57
Hsi <its.hsichen@gmail.com>
2026-05-20 23:37:16 +0800

    refactor: use index for global css

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 
 import { App } from './App.js';
 
-import './styles/global.css';
+import './index.css';
 
 const queryClient = new QueryClient();
 const rootElement = document.querySelector('#root');
